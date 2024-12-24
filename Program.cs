43d152fc613ed959634c7be82c716701bc3d47a8
Сергей Кущян <95130963+serojghushch@users.noreply.github.com>
2024-12-24 10:48:02 +0300

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using kushchyan_biblioteka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kushchyan_consolprilozh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person("Сергей", "Улица Примерная, 1");
+
+            //Кушян Сергей 34 ИС
+            person.AddHobby(new Hobby("Чтение", "Чтение книг различных жанров"));
+            person.AddHobby(new Hobby("Путешествия", "Посещение новых стран и городов"));
+            person.AddHobby(new Hobby("Спорт", "Занятия фитнесом и бегом"));
+
+
+            person.Show();
+
+
+            person.ShowAll();
+            Console.ReadKey();
+        }
+    }
+}
