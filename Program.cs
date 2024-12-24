8e57a95027353fa4605d5c6feda394af27cba053
Сергей Кущян <95130963+serojghushch@users.noreply.github.com>
2024-12-24 11:10:22 +0300

    Второй коммит

--- Program.cs
+++ Program.cs
@@ -19,11 +19,28 @@ namespace kushchyan_consolprilozh
             person.AddHobby(new Hobby("Путешествия", "Посещение новых стран и городов"));
             person.AddHobby(new Hobby("Спорт", "Занятия фитнесом и бегом"));
 
+            Person person1 = new Person("Кирилл", "Улица Примерная, 1");
+
+            //Кушян Сергей 34 ИС
+            person1.AddHobby(new Hobby("Чтение", "Чтение книг различных жанров"));
+            person1.AddHobby(new Hobby("Путешествия", "Посещение новых стран и городов"));
+            person1.AddHobby(new Hobby("Спорт", "Занятия фитнесом и бегом"));
+            Person person2 = new Person("Максим", "Улица Примерная, 1");
+
+            //Кушян Сергей 34 ИС
+            person2.AddHobby(new Hobby("Чтение", "Чтение книг различных жанров"));
+            person2.AddHobby(new Hobby("Путешествия", "Посещение новых стран и городов"));
+            person2.AddHobby(new Hobby("Спорт", "Занятия фитнесом и бегом"));
+
 
             person.Show();
+            person1.Show();
+            person2.Show();
 
 
             person.ShowAll();
+            person1.ShowAll();
+            person2.ShowAll();
             Console.ReadKey();
         }
     }
